58f6be5b8d68811b2e511bbac20f89d3ffc538fe
Appulcake <metroid4.metroid4@gmail.com>
2026-09-21 23:42:03 +0200

    Version bump that was forgotten in previous release, and new TailhookOnLandingGearFix

---
+++ PRF/Fixes/TailhookOnLandingGearFix.cs
@@ -0,0 +1,79 @@
+#if CLIENT
+using System.Linq;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace PRF.Fixes;
+
+[Fix]
+[HarmonyPatch]
+internal class TailhookOnLandingGearFix(ConfigFile config) : ConfigurableFix(config)
+{
+    private const float MinimumDeployRadarAltitude = 1f;
+    
+    protected override string Description =>
+        $"{base.Description}\nTies tail hook state to landing gear, when you deploy gear tail hook also deploys, and " +
+        "when retracting gear it also retracts.";
+    
+    [HarmonyPatch(typeof(TailHook), nameof(TailHook.CheckDeployConditions))]
+    [HarmonyPrefix]
+    // ReSharper disable once InconsistentNaming
+    private static bool CheckDeployConditionsPrefix(TailHook __instance)
+    {
+        if (__instance.unitPart?.parentUnit is Aircraft aircraft)
+            UpdateTailHook(__instance, aircraft, GetDesiredState(aircraft));
+        
+        return false;
+    }
+    
+    [HarmonyPatch(typeof(Aircraft), nameof(Aircraft.SetGear), typeof(LandingGear.GearState))]
+    [HarmonyPostfix]
+    // ReSharper disable once InconsistentNaming
+    private static void SetGearPostfix(Aircraft __instance, LandingGear.GearState gearState)
+    {
+        var deploy = GetDesiredState(__instance, gearState);
+        var tailHooks = __instance.GetAllParts()
+            .SelectMany(part => part.GetComponentsInChildren<TailHook>(true));
+        
+        foreach (var tailHook in tailHooks)
+            UpdateTailHook(tailHook, __instance, deploy);
+    }
+    
+    private static bool GetDesiredState(Aircraft aircraft) => GetDesiredState(aircraft, aircraft.gearState);
+    
+    private static bool GetDesiredState(Aircraft aircraft, LandingGear.GearState gearState)
+    {
+        return gearState switch
+        {
+            LandingGear.GearState.Extending => true,
+            LandingGear.GearState.LockedExtended => true,
+            
+            LandingGear.GearState.Retracting => false,
+            LandingGear.GearState.LockedRetracted => false,
+            
+            _ => aircraft.gearDeployed
+        };
+    }
+    
+    private static void UpdateTailHook(TailHook tailHook, Aircraft aircraft, bool deploy)
+    {
+        // This is to prevent it deploying from retracted state on the ground (e.g. when you spawn so it's not out)
+        if (deploy && !tailHook.deployed && aircraft.radarAlt <= MinimumDeployRadarAltitude)
+            return;
+        
+        if (tailHook.deployed == deploy)
+            return;
+        
+        tailHook.deployed = deploy;
+        
+        if (!deploy)
+            tailHook.Unhook();
+        
+        tailHook.enabled = true;
+        
+        if (GameManager.IsLocalAircraft(aircraft))
+            SceneSingleton<AircraftActionsReport>.i.ReportText(deploy ? "Tail Hook Deployed" : "Tail Hook Retracted",
+                4f);
+    }
+}
+#endif
